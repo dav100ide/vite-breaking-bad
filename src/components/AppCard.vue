01a0f459e67226bf518a31c4bf0b9ba7498468a8
dav100ide <oreglio.dev@gmail.com>
2022-11-28 16:45:54 +0100

    stampo tramite api le card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,22 +1,22 @@
 <template>
-   <div>card</div>
+   <div class="ms_card">
+      <img :src="characterInfo.img" :alt="characterInfo.name" class="img-fluid" />
+      <h3>{{ characterInfo.name }}</h3>
+      <div class="series">Breaking Bad</div>
+      <div class="status">{{ characterInfo.status }}</div>
+   </div>
 </template>
 
 <script>
 export default {
-   data() {
-      return {
-         characters: [],
-      };
-   },
-   created() {
-      axios
-         .get('https://www.breakingbadapi.com/api/characters') //
-         .then((response) => {
-            console.log(response);
-         });
+   props: {
+      characterInfo: Object,
    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ms_card {
+   background-color: steelblue;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,37 @@
 <template>
-   <main class="ms_container">
-      <AppCard />
+   <main class="ms_container row">
+      <AppCard class="col-6 col-md-4 col-lg-3" v-for="character in charactersInfo" :characterInfo="character" />
    </main>
 </template>
 
 <script>
 import AppCard from './AppCard.vue';
+import axios from 'axios';
+
 export default {
    components: {
       AppCard,
+   },
+
+   data() {
+      return {
+         charactersInfo: [],
+      };
+   },
+   created() {
+      axios
+         .get('https://www.breakingbadapi.com/api/characters') //
+         .then((response) => {
+            this.charactersInfo = response.data;
+            console.log(response.data[2]);
+         });
    },
 };
 </script>
 
 <style lang="scss" scoped>
 .ms_container {
-   max-width: 1000px;
+   max-width: 1100px;
    margin: 0 auto;
 }
 </style>
