22582a95f0dfe5fac5f55124ef64be6369dd8d6b
dav100ide <oreglio.dev@gmail.com>
2022-11-29 18:02:46 +0100

    aggiunge option bb-better-call

--- src/components/SelectSection.vue
+++ src/components/SelectSection.vue
@@ -2,6 +2,7 @@
    <div class="ms_container text-center mb-2">
       <label class="me-2" for="series">scegli categoria</label>
       <select class="p-1 rounded-3" name="series" id="series" @change="changeCategory">
+         <option value="bb-better-call">breaking bad and better call</option>
          <option value="breaking-bad">Breaking Bad</option>
          <option value="better-call">Better Call Saul</option>
       </select>
@@ -25,13 +26,17 @@ export default {
          if (option === 'breaking-bad') {
             axios.get('https://www.breakingbadapi.com/api/characters?category=Breaking+Bad').then((response) => {
                this.store.charactersInfo = response.data;
-               console.log(this.store.charactersInfo);
             });
          } else if (option === 'better-call') {
             axios.get('https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul').then((response) => {
                this.store.charactersInfo = response.data;
-               console.log(this.store.charactersInfo);
             });
+         } else if (option === 'bb-better-call') {
+            axios
+               .get('https://www.breakingbadapi.com/api/characters') //
+               .then((response) => {
+                  this.store.charactersInfo = response.data;
+               });
          }
       },
    },
